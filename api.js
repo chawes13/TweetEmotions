55b21132108a6b47b512cc14bd8c53f30c6e8562
chawes <hawes.conor@gmail.com>
2018-05-07 16:19:54 -0500

    Refactored using plain promises

--- api.js
+++ api.js
@@ -5,25 +5,36 @@ const API_KEY = '604b7c058187eb91da2f60b7aabc1af3';
 const TRACK_SEARCH = `http://api.musixmatch.com/ws/1.1/track.search?apikey=${API_KEY}&q_lyrics=`;
 const LYRICS_SEARCH = `http://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=${API_KEY}&track_id=`;
 
-const getLyrics = async (keyword) => {
-  try {
-    const response = await axios.get(TRACK_SEARCH+keyword);
-    const tracks = response.data.message.body['track_list'];
-    const randomIndex = Math.floor(Math.random()*tracks.length);
-    const randomTrackId = tracks[randomIndex].track['track_id'];
+const getLyrics = (keyword) => {
+  let tracks;
+  let randomIndex;
+  let randomTrackId;
+  let lyrics;
+  let stopIndex;
 
-    const lyricsResponse = await axios.get(LYRICS_SEARCH+randomTrackId);
-    let lyrics = lyricsResponse.data.message.body.lyrics['lyrics_body'];
-    let stopIndex = lyrics.indexOf('...');
+  axios.get(TRACK_SEARCH+keyword)
+    .then(response => {
+      tracks = response.data.message.body['track_list'];
+      randomIndex = Math.floor(Math.random()*tracks.length);
+      randomTrackId = tracks[randomIndex].track['track_id'];
 
-    if (stopIndex > 75) stopIndex = 75;
-    lyrics = lyrics.substring(0, stopIndex).replace('\n', ' ');
+      return randomTrackId;
+    })
+    .then(trackId => {
+      return axios.get(LYRICS_SEARCH+trackId);
+    })
+    .then(lyricsResponse => {
+      lyrics = lyricsResponse.data.message.body.lyrics['lyrics_body'];
+      stopIndex = lyrics.indexOf('...');
+      if (stopIndex > 75) stopIndex = 75;
+      lyrics = lyrics.substring(0, stopIndex).replace('\n', ' ');
 
-    console.log(lyrics);
-    return lyrics;
-  } catch (error) {
-    console.error(error);
-  }
+      console.log(lyrics);
+      return lyrics;
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
 
 getLyrics('angry');
